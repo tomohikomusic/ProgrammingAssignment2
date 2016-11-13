4a2cedabd0332e0f17d1b98757ad686013c9cc7b
Tomohiko Music <tomohiko.music@outlook.com>
2016-11-13 15:45:54 -0500

    cachematrix assignment

--- cachematrix.R
+++ cachematrix.R
@@ -1,15 +1,37 @@
-## Put comments here that give an overall description of what your
-## functions do
+## The following functions demonstrates R's lexical scoping properties and the ability to create functions
+## that also act as objects to other functions. R's scoping properties give a function access to both the
+## objects within the list created by makeCacheMatrix as well as the original object passed through the 
+## function. This is handy in this case because it allows us to retrieve cached data rather than run 
+## the function again.
 
-## Write a short comment describing this function
+## This function takes a solvable square matrix as an object and outputs a list of functions accessible to
+## to the cacheSolve function. It allows values to be captured and saved in cache to be accessed later.
 
 makeCacheMatrix <- function(x = matrix()) {
-
+  m<-NULL
+  set<-function(y){
+    x<<-y
+    m<<-NULL
+  }
+  get<-function()x
+  setinv<-function(solve) m<<-solve
+  getinv<-function()m
+  list(set=set,get=get,setinv=setinv,getinv=getinv)
 }
 
 
-## Write a short comment describing this function
+## This function uses makeCacheMatrix as an object. If a value for the inverse of the original object x was
+## already computed, this function retrieves that value and returns it. If there is no value, then this 
+## function proceeds to calculate the inverse of the original matrix object and returns the value.
 
 cacheSolve <- function(x, ...) {
-        ## Return a matrix that is the inverse of 'x'
+  m<-x$getinv()
+  if(!is.null(m)){
+    message("getting cached data")
+    return(m)
+  }
+  data<-x$get()
+  m<-solve(data,...)
+  x$setinv(m)
+  m
 }
